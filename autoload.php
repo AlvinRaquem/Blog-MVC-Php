05f42097e615b264acc3348a2490002a00cb74f4
Alvin S. Raquem <41246806+AlvinRaquem@users.noreply.github.com>
2018-12-31 11:39:29 +0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,13 +1,12 @@
 <?php
 
 
-function __autoload($class){
+spl_autoload_register(function($class){
 	if(file_exists(APP_PATH.$class.".php")){
 		require APP_PATH.$class.".php";
 	}else if(file_exists(BASE_PATH.$class.".php")){
 		require BASE_PATH.$class.".php";
 	}
-	
-}
+});
 
 
