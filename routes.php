0ba54e7b4828d4f4c3f1c01f44dc6f4bde479dda
Alvin S. Raquem <41246806+AlvinRaquem@users.noreply.github.com>
2018-12-31 11:55:30 +0800

    Update routes.php

--- routes.php
+++ routes.php
@@ -9,38 +9,49 @@ use controller\usercontroller;
 Route::make('index.php',function(){
 	$blog = new blogcontroller;
 	$blog->index();
+	exit;
 });
 
 Route::make('viewblog',function(){
 	$blog = new blogcontroller;
 	$blog->view();
+	exit;
 });
 
 Route::make('add',function(){
 	$blog = new blogcontroller;
  	$blog->add();
+ 	exit;
 });
 
 Route::make('login',function(){
 	view::make("login");
+	exit;
 });
 
 Route::make('checklogin',function(){
 	$user = new usercontroller;
  	$user->checkuser();
+ 	exit;
 });
 
 Route::make('remove',function(){
 	$blog = new blogcontroller;
  	$blog->remove($_GET['idno']);
+ 	exit;
 });
 
 Route::make('addblog',function(){
 	$blog = new blogcontroller;
 	$blog->save();
+	exit;
 });
 
 Route::make('about',function(){
 	view::make("about");
+	exit;
 });
+
+// if url is not in list will display this view
+view::make('notexist');
 
